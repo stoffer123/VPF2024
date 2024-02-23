1afa98e62db63b5db6dd8887f52c88ded54d0d6a
stoffer123 <christophermikkelsen@live.dk>
2024-02-23 17:04:02 +0100

    added board.makeMove()

--- Projects/FirstSteps/src/main/java/dk/cphbusiness/evu/vp/f2024/tictactoe/demo/GameDemo.java
+++ Projects/FirstSteps/src/main/java/dk/cphbusiness/evu/vp/f2024/tictactoe/demo/GameDemo.java
@@ -1,12 +1,22 @@
 package dk.cphbusiness.evu.vp.f2024.tictactoe.demo;
 
 import dk.cphbusiness.evu.vp.f2024.tictactoe.Board;
+import dk.cphbusiness.evu.vp.f2024.tictactoe.GameCtrl;
+import dk.cphbusiness.evu.vp.f2024.tictactoe.Player;
 import dk.cphbusiness.evu.vp.f2024.tictactoe.impl.BoardImpl;
+import dk.cphbusiness.evu.vp.f2024.tictactoe.impl.GameCtrlImpl;
+import dk.cphbusiness.evu.vp.f2024.tictactoe.impl.PlayerImpl;
 
 public class GameDemo
 {
     public static void main(String[] args) {
         Board board = new BoardImpl();
-        board.printBoard();
+        Player player1 = new PlayerImpl("Spiller1", 'X');
+        Player player2 = new PlayerImpl("Spiller2", 'O');
+        GameCtrl gameCtrl = new GameCtrlImpl();
+
+        gameCtrl.runGame(board, player1, player2);
+
+
     }
 }
